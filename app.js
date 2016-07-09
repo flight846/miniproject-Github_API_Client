e07f2017f91e8066f145139b4ad2bc4df8f6c222
Yazid Ismail <flight846@gmail.com>
2016-07-10 01:35:11 +0800

    Add repo count api call

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 console.clear()
 
+//$('#search').click(function(){
+//    window.location.reload();
+//})
+
 var searchUser = $('#search');
 
 function reload() {
@@ -8,14 +12,23 @@ function reload() {
         $.get('https://api.github.com/users/'+user+'/repos')
             .done(function(data){
                 data.forEach( function(datum) {
-                    $('#repos').append('<li class="list-group-item"><a href="'+datum.html_url+'">'+datum.name+'</li>')
+                    $('#repos').append('<li class="list-group-item"><a href="'+datum.html_url+'" target="_blank">'+datum.name+'</li>')
                 })
 //                console.log(data);
             }).fail(function(jqXHR, textStatus, errorThrown){
-                console.log(errorThrown)
+                alert('Failed to load resource: the server responded with a status of 404 ('+textStatus+')')
                 });
+    
+        // Get user stats
+        $.get('https://api.github.com/users/'+user+'')
+            .done(function(data){
+            $('.profile-pic').remove();
+            $('#avatar').append('<img class="profile-pic" src="'+data.avatar_url+'">');
+            $('#repo-count').html(''+data.public_repos+' <br/>Repos');
+        })
 }
 
 $('#search-users').click(function(){
+//    window.location.reload();
     reload();
 })
