a89eaf600f90f0ba948434b48398f48864853e9d
Yazid Ismail <flight846@gmail.com>
2016-07-10 13:22:13 +0800

    Add user stats api call update and refresh button

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ function reload() {
                 data.forEach( function(datum) {
                     $('#repos').append('<li class="list-group-item"><a href="'+datum.html_url+'" target="_blank">'+datum.name+'</li>')
                 })
+                
 //                console.log(data);
             }).fail(function(jqXHR, textStatus, errorThrown){
                 alert('Failed to load resource: the server responded with a status of 404 ('+textStatus+')')
@@ -25,10 +26,20 @@ function reload() {
             $('.profile-pic').remove();
             $('#avatar').append('<img class="profile-pic" src="'+data.avatar_url+'">');
             $('#repo-count').html(''+data.public_repos+' <br/>Repos');
+            $('#create-date').html('<strong>Updated at: <span>'+data.created_at+'</span></strong>');
+            $('#update-date').html('<strong>Updated at: <span>'+data.updated_at+'</span></strong>');
+        })
+        
+        // Get activity count
+        $.get('https://api.github.com/users/'+user+'/received_events')     .done(function(data){
+                $('#activity-count').html(''+data.length+' <br/>Activities');
         })
 }
 
 $('#search-users').click(function(){
-//    window.location.reload();
     reload();
+})
+
+$('#new-user, #octodash').click(function(){
+    window.location.reload();
 })
